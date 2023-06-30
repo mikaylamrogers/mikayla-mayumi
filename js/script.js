9e049bd9ccb10de727724a7d1753d75bacebf116
Mikayla Mayumi <58568794+mikaylamrogers@users.noreply.github.com>
2023-06-29 22:07:56 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,46 +4,52 @@ let contentGridElement = document.getElementById('contentGrid');
 let jsonDatabase = [
     {
       "title" : "Megumi Organics",
-      "picture_url" : "gifs/megumi.gif",
+      "picture_url" : "pngs/megumi.png",
       "link" : "html/megumi-organics.html",
-      "category" : "Web Dev · Freelance",
-    },
-    {
-      "title" : "You Are Invited to Breakup",
-      "picture_url" : "gifs/breakup.png",
-      "link" : "html/you-are-invited-to-breakup.html",
-      "category" : "Web Dev",
-    },
-    {
-      "title" : "Halo Halo",
-      "picture_url" : "gifs/halohalo.gif",
-      "link" : "html/halo-halo.html",
-      "category" : "Web Dev",
-    },
-    {
-      "title" : "For Your Viewing Pleasure",
-      "picture_url" : "gifs/foryour.gif",
-      "link" : "html/for-your-viewing-pleasure.html",
-       "category" : "Web Dev",
+      "category" : "DEVELOPMENT · DESIGN",
     },
     {
       "title" : "光 / Licht / Light",
-      "picture_url" : "gifs/light.gif",
+      "picture_url" : "pngs/light.png",
       "link" : "html/light.html",
-      "category" : "Web Dev",
+      "category" : "DEVELOPMENT · DESIGN",
     },
     {
       "title" : "Mecha Mecca",
-      "picture_url" : "gifs/mecha.gif",
+      "picture_url" : "pngs/mecha.png",
       "link" : "html/mechamecca.html",
-      "category" : "Web Dev",
+      "category" : "DEVELOPMENT · DESIGN",
     },
     {
-      "title" : "Loop Wellness",
-      "picture_url" : "gifs/loop.gif",
-      "link" : "html/loop-wellness.html",
-      "category" : "Web Dev",
+      "title" : "For Your Viewing Pleasure",
+      "picture_url" : "pngs/for.png",
+      "link" : "html/for-your-viewing-pleasure.html",
+       "category" : "DEVELOPMENT · DESIGN",
     },
+    {
+      "title" : "Webcam Waterfall",
+      "picture_url" : "pngs/waterfall.png",
+      "link" : "html/webcam-waterfall.html",
+      "category" : "Creative Code",
+  },
+    {
+      "title" : "Halo Halo",
+      "picture_url" : "pngs/halo.png",
+      "link" : "html/halo-halo.html",
+      "category" : "DEVELOPMENT · DESIGN",
+    },
+    // {
+    //   "title" : "You Are Invited to Breakup",
+    //   "picture_url" : "gifs/breakup.png",
+    //   "link" : "html/you-are-invited-to-breakup.html",
+    //   "category" : "DEVELOPMENT · DESIGN",
+    // },
+    // {
+    //   "title" : "Loop Wellness",
+    //   "picture_url" : "gifs/loop.gif",
+    //   "link" : "html/loop-wellness.html",
+    //   "category" : "Web Dev",
+    // },
     // {
     //   "title" : "Speggtators",
     //   "picture_url" : "gifs/speggtators.gif",
@@ -56,77 +62,71 @@ let jsonDatabase = [
     //   "link" : "html/weather-app.html",
     //   "category" : "Web Dev",
     // },
-    {
-      "title" : "Wheel of Songs",
-      "picture_url" : "gifs/wheelofsongs.gif",
-      "link" : "html/wheel-of-songs.html",
-      "category" : "Web Dev",
-    },
-    {
-      "title" : "Puddle Study",
-      "picture_url" : "gifs/puddlestudy.gif",
-      "link" : "html/puddle-study.html",
-      "category" : "Web Dev",
-    },
+    // {
+    //   "title" : "Wheel of Songs",
+    //   "picture_url" : "gifs/wheelofsongs.gif",
+    //   "link" : "html/wheel-of-songs.html",
+    //   "category" : "Web Dev",
+    // },
+    // {
+    //   "title" : "Puddle Study",
+    //   "picture_url" : "gifs/puddlestudy.gif",
+    //   "link" : "html/puddle-study.html",
+    //   "category" : "Web Dev",
+    // },
     {
         "title" : "Self Portrait Stamp",
-        "picture_url" : "gifs/stamp.gif",
+        "picture_url" : "pngs/stamp.png",
         "link" : "html/portrait-stamp.html",
         "category" : "Creative Code",
     },
-    {
-        "title" : "Webcam Waterfall",
-        "picture_url" : "gifs/waterfall.gif",
-        "link" : "html/webcam-waterfall.html",
-        "category" : "Creative Code",
-    },
-    {
-      "title" : "If I Smushed My Face Into a Van Gogh",
-      "picture_url" : "gifs/smushed.gif",
-      "link" : "html/if-i-smushed-my-face.html",
-      "category" : "Creative Code",
-    },
+    // {
+    //   "title" : "If I Smushed My Face Into a Van Gogh",
+    //   "picture_url" : "gifs/smushed.gif",
+    //   "link" : "html/if-i-smushed-my-face.html",
+    //   "category" : "Creative Code",
+    // },
     // {
     //   "title" : "Legumes and Portraits",
     //   "picture_url" : "gifs/legumes.gif",
     //   "category" : "Creative Code",
     // },
-    {
-      "title" : "Bong Bong and Bongs",
-      "picture_url" : "gifs/bongs.gif",
-      "link" : "html/bong-bong-bongs.html",
-      "category" : "Creative Code",
-    },
-    {
-      "title" : "MAIA App",
-      "picture_url" : "gifs/maia.gif",
-      "link" : "html/maia.html",
-      "category" : "UI/UX",
-    },
-    {
-      "title" : "WTF (What the Font)",
-      "picture_url" : "gifs/wtf.gif",
-      "link" : "html/wtf.html",
-      "category" : "UI/UX",
-    },
-    {
-      "title" : "IRIS",
-      "picture_url" : "gifs/iris.gif",
-      "link" : "html/iris.html",
-      "category" : "UI/UX",
-    },
+    // {
+    //   "title" : "Bong Bong and Bongs",
+    //   "picture_url" : "gifs/bongs.gif",
+    //   "link" : "html/bong-bong-bongs.html",
+    //   "category" : "Creative Code",
+    // },
+    // {
+    //   "title" : "MAIA App",
+    //   "picture_url" : "gifs/maia.gif",
+    //   "link" : "html/maia.html",
+    //   "category" : "UI/UX",
+    // },
+    // {
+    //   "title" : "WTF (What the Font)",
+    //   "picture_url" : "gifs/wtf.gif",
+    //   "link" : "html/wtf.html",
+    //   "category" : "UI/UX",
+    // },
+    // {
+    //   "title" : "IRIS",
+    //   "picture_url" : "gifs/iris.gif",
+    //   "link" : "html/iris.html",
+    //   "category" : "UI/UX",
+    // },
     {
       "title" : "Summer of '93",
-      "picture_url" : "gifs/summer.gif",
+      "picture_url" : "pngs/summer.png",
       "link" : "html/summer-of-93.html",
-      "category" : "Motion Graphics",
-    },
-    {
-      "title" : "Freddie Juggling Through Paris",
-      "picture_url" : "gifs/freddie.gif",
-      "link" : "html/freddie.html",
-      "category" : "Motion Graphics",
+      "category" : "Motion Design",
     }
+    // {
+    //   "title" : "Freddie Juggling Through Paris",
+    //   "picture_url" : "gifs/freddie.gif",
+    //   "link" : "html/freddie.html",
+    //   "category" : "Motion Design",
+    // }
 ]
   
 for (var i = 0; i < jsonDatabase.length; i++ ) {
@@ -137,33 +137,32 @@ for (var i = 0; i < jsonDatabase.length; i++ ) {
 function createElementProper(incomingJSON) {
 let newContentElement = document.createElement("DIV");
 
+// ADD TITLE AND CATEGORY CONTAINER //
+let titleContainer = document.createElement("div");
+titleContainer.style.display = "flex";
+titleContainer.style.justifyContent = "space-between";
+titleContainer.style.alignItems = "center";
+
 // ADD TITLE //
 let newContentHeading = document.createElement("H3");
-newContentHeading.style.rotate = "24.82deg";
-newContentHeading.style.paddingBottom = "2%";
-newContentHeading.style.textAlign = "center";
+newContentHeading.style.textAlign = "left";
+newContentHeading.textContent = incomingJSON['title'];
+titleContainer.appendChild(newContentHeading);
 
-newContentHeading.innerHTML = "<a href='" + incomingJSON['link'] + "'>" + incomingJSON['title'] + "</a>";
-newContentElement.appendChild(newContentHeading);
+// ADD CATEGORY //
+let newContentSubheading = document.createElement("H5");
+newContentSubheading.style.textAlign = "center";
+newContentSubheading.textContent = incomingJSON['category'];
+titleContainer.appendChild(newContentSubheading);
 
-// change color on mouseover
-newContentHeading.addEventListener("mouseover", function(){
-    newContentHeading.style.color = "rgb(5, 38, 255)";
-    newContentSubheading.style.transform = "scale(1.2)";
-    newContentHeading.style.transition = "0.5s";
-})
-newContentHeading.addEventListener("mouseout", function(){
-    newContentHeading.style.color = "black";
-    newContentSubheading.style.transform = "scale(1)";
-})
+newContentElement.appendChild(titleContainer);
   
 // ADD IMAGES //
 let newImage = document.createElement("IMG");
 newImage.classList.add("profilePicture");
-newImage.style.rotate = "24.82deg";
 newImage.style.width = "100%";
-// newImage.style.height = "auto";
-newImage.style.boxShadow = "10px 10px 5px #ccc";
+// newImage.style.height = "80%";
+// newImage.style.boxShadow = "5px 5px 5px #ccc";
 
 // newImage.innerHTML = "<a href='" + incomingJSON['link'] + "'>" + "<img src='" + incomingJSON['picture_url'] + "'>" + "</a>";
 newImage.src = incomingJSON['picture_url'];
@@ -178,33 +177,13 @@ contentGridElement.appendChild(newContentElement);
 
 // rotate images on mouseover
 newImage.addEventListener("mouseover", function(){
-    newImage.style.transform = "rotate3d(-1, 1, 0, 45deg)";
+    // newImage.style.transform = "rotate3d(-1, 1, 0, 45deg)";
     newImage.style.transition = "1s";
 })
 newImage.addEventListener("mouseout", function(){
-    newImage.style.transform = "rotate3d(0, 0, 0, 0deg)";
+    // newImage.style.transform = "rotate3d(0, 0, 0, 0deg)";
 })
-  
-// ADD CATEGORY //
-let newContentSubheading = document.createElement("H5");
-newContentSubheading.style.rotate = "24.82deg";
-newContentSubheading.style.textAlign = "left";
-newContentSubheading.style.paddingTop = "3%";
-// newContentSubheading.style.paddingBottom = "15%";
 
-newContentSubheading.innerHTML = incomingJSON['category'];
-
-newContentElement.appendChild(newContentSubheading);
-// change color on mouseover
-newContentSubheading.addEventListener("mouseover", function(){
-    newContentSubheading.style.color = "rgb(2, 29, 207)";
-    newContentSubheading.style.transform = "scale(1.2)";
-    newContentSubheading.style.transition = "1s";
-})
-newContentSubheading.addEventListener("mouseout", function(){
-    newContentSubheading.style.color = "black";
-    newContentSubheading.style.transform = "scale(1)";
-})
 
 // NAV BAR
 const all = document.getElementById("all");
